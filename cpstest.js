f108846a315417a0153ccb592f6705e2c6fcc710
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2021-05-21 15:36:48 -0400

    Add files via upload

---
+++ cpstest.js
@@ -0,0 +1,92 @@
+const {log} = console
+
+function summonPopup(elem){
+  if(!elem){
+    return;
+  }
+  
+elem.classList.add('popup')
+elem.classList.remove('futurePopup')
+  
+  setTimeout(function() {
+elem.classList.remove('popup');
+elem.classList.add('futurePopup')
+  },8500)
+}
+function dataAttr(str,i = null){
+  
+ return i === null ? document.querySelectorAll('[data-'+str+']') : document.querySelectorAll('[data-'+str+']')[i]
+}
+
+if('banned' in localStorage){
+   close()
+}
+
+      var clicks = 0;
+       
+    function handleClick(){
+    clicks++
+
+    dataAttr('num',0).textContent = 'Clicks: '+clicks
+    }
+
+    setTimeout(() => {
+dataAttr('click',0).removeEventListener('click',handleClick)
+
+dataAttr('future',0).textContent = 'Done!'
+        
+        summonPopup(dataAttr('future',0))
+
+let CPS = clicks/5
+
+if(CPS >= 8){
+    localStorage.setItem('banned',true)
+
+        dataAttr('future',0).textContent = 'You\'re a cheater!'
+        
+        summonPopup(dataAttr('future',0))
+    
+   close()
+}
+        
+if('highscore' in localStorage){
+  if(+localStorage.getItem('highscore') >= 8){
+    localStorage.setItem('banned',true);
+
+      dataAttr('future',0).textContent =   'I see that you cheated to get your highscore #banned.'
+        
+        summonPopup(dataAttr('future',0))
+          
+    close()
+  }
+   else if(CPS > +localStorage.getItem('highscore')){
+ localStorage.setItem('highscore',CPS)
+    dataAttr('future',0).textContent = 'New HighScore: '+CPS
+        
+        summonPopup(dataAttr('future',0))
+       
+       document.body.innerHTML += "<br/><br/><button id='refresh' onclick='location.reload()'>Refresh the Page!</button>"
+}
+    else{
+        dataAttr('future',0).textContent = 'Your score is '+CPS
+        
+        summonPopup(dataAttr('future',0))
+
+              document.body.innerHTML += "<br/><br/><button id='refresh' onclick='location.reload()'>Refresh the Page!</button>"
+    }
+}
+else{
+    dataAttr('future',0).textContent = 'Your score is '+CPS
+        
+        summonPopup(dataAttr('future',0));
+    
+    localStorage.setItem('highscore',CPS)
+           document.body.innerHTML += "<br/><br/><button id='refresh' onclick='location.reload()'>Refresh the Page!</button>"
+}
+},5000)
+        dataAttr('click',0).addEventListener('click',handleClick)
+document.addEventListener('keypress',function(e){
+    if((e.code.toLowerCase() === 'space' || e.code.toLowerCase() === 'enter') && !e.altKey && !e.ctrlKey && !e.shiftKey && !e.metaKey){
+     e.preventDefault()
+    }
+})
